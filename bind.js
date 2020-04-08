abc6c3543a6cbca34d5d00c75a14750afe46cffd
fu wei <31303117+fuwei-1993@users.noreply.github.com>
2020-04-08 16:00:13 +0800

    feat: create a bind function

---
+++ bind.js
@@ -0,0 +1,43 @@
+function Animal(name, color) {
+  this.name = name
+  this.color = color
+  console.log(this)
+}
+
+Animal.prototype.say = function () {
+  return `I'm a ${this.color} ${this.name}`
+}
+
+Animal.apply = function (target, arg) {
+  if (target && typeof target === 'object') {
+    target.fn = this
+    const result = target.fn(...arg)
+    delete target.fn
+    return result
+  }
+  return this(...arg)
+}
+
+Function.prototype.myBind = function (target, arg) {
+  const that = this
+
+  function result(...args) {
+    const isNew = this instanceof result
+    return that.apply(isNew ? this : target, [arg, ...args])
+  }
+
+  result.prototype = Object.create(this.prototype)
+  return result
+}
+
+const Cat = Animal.myBind({}, 'cat')
+
+const cat = new Cat('white')
+
+if (
+  cat.say() === "I'm a white cat" &&
+  cat instanceof Cat &&
+  cat instanceof Animal
+) {
+  console.log('success')
+}
