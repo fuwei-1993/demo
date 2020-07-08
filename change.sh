3ba77d5169b32b20368228a8283fbd5909146ddf
fuwei <454575238@qq.com>
2020-07-08 18:53:18 +0800

    fix: fix the bash

--- change.sh
+++ change.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git filter-branch --env-filter ' OLD_EMAIL="wb-fw665529@antfin.com" CORRECT_NAME="fuwei" CORRECT_EMAIL="454575238@qq.com" if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ] then     export GIT_COMMITTER_NAME="$CORRECT_NAME"     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL" fi if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ] then     export GIT_AUTHOR_NAME="$CORRECT_NAME"     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL" fi ' --tag-name-filter cat -- --branches --tags
+git filter-branch --env-filter ' OLD_EMAIL="<wb-fw665529@antfin.com>" CORRECT_NAME="fuwei" CORRECT_EMAIL="<454575238@qq.com>" if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ] then     export GIT_COMMITTER_NAME="$CORRECT_NAME"     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL" fi if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ] then     export GIT_AUTHOR_NAME="$CORRECT_NAME"     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL" fi ' --tag-name-filter cat -- --branches --tags
