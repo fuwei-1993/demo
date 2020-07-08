b317aaa40e441eb35bd4f984dffcd8b8b8fa3872
fuwei <454575238@qq.com>
2020-07-08 19:01:48 +0800

    fix: replaced the shell

--- change.sh
+++ change.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
+ 
+git filter-branch --env-filter '
+ 
+an="$GIT_AUTHOR_NAME"
+am="$GIT_AUTHOR_EMAIL"
+cn="$GIT_COMMITTER_NAME"
+cm="$GIT_COMMITTER_EMAIL"
+ 
+if [ "$GIT_COMMITTER_EMAIL" = "[wb-fw665529@antfin.com]" ]
+then
+    cn="[fuwei]"
+    cm="[454575238@qq.com]"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "[wb-fw665529@antfin.com]" ]
+then
+    an="[fuwei]"
+    am="[454575238@qq.com]"
+fi
+ 
+export GIT_AUTHOR_NAME="$an"
+export GIT_AUTHOR_EMAIL="$am"
+export GIT_COMMITTER_NAME="$cn"
+export GIT_COMMITTER_EMAIL="$cm"
 
-git filter-branch --env-filter ' OLD_EMAIL="<wb-fw665529@antfin.com>" CORRECT_NAME="fuwei" CORRECT_EMAIL="<454575238@qq.com>" if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ] then     export GIT_COMMITTER_NAME="$CORRECT_NAME"     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL" fi if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ] then     export GIT_AUTHOR_NAME="$CORRECT_NAME"     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL" fi ' --tag-name-filter cat -- --branches --tags
+
+'
