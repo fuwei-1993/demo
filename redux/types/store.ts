fc4aec82d865eaf93070cbed2e960aa0f18facdd
fuwei <454575238@qq.com>
2020-08-05 05:33:44 +0800

    feat(redux): add symbol.observable

--- redux/types/store.ts
+++ redux/types/store.ts
@@ -1,10 +1,11 @@
+import { Action, AnyAction } from './action'
+import '../utils/symbol-observable'
 /**
  * Extend the state
  *
  * If there is no state extension, it just returns the state,
  * as is, otherwise it returns the state joined with its extension
  */
-
 export type ExtendState<State, Extension> = [Extension] extends [never] // 没看懂这个中括号的含义，感觉可以省略
   ? State
   : State & Extension
@@ -18,7 +19,6 @@ declare const $CombinedState: unique symbol
  * It's too long to bother to write
  */
 export type CombinedState<S> = { readonly [$CombinedState]?: undefined } & S
-
 
 /**
  * 这个写的是啥 ** 玩意儿
@@ -36,3 +36,38 @@ export type PreloadedState<S> = Required<S> extends {
         ? S[K]
         : PreloadedState<S[K]>
     }
+
+/**
+ * @template A The type of things (actions or otherwise) which may be
+ * despatch
+ */
+export interface Dispatch<A extends Action = AnyAction> {
+  <T extends A>(action: T, ...extraArgs: any[]): T
+}
+
+/**
+ *  Function to remove listener added by `Store.subscribe`
+ */
+export interface Unsubscribe {
+  (): void
+}
+
+/**
+ * An Observer is used to receive data from an Observable, and is supplied
+ * an argument to subscribe
+ */
+export type Observer<T> = {
+  next?(value: T): void
+}
+
+export type Observable<T> = {
+  /**
+   * @param {Object} observer Any object that can be used as an observer
+   * The observer object should have a `next` method
+   * @return {subscription} An object with an `unsubscribe` that can be used
+   * to unsubscribe the observable from the store, and prevent further
+   * emission of values from the observable
+   */
+  subscribe: (observer: Observer<T>) => { unsubscribe: Unsubscribe } 
+  [Symbol.observable](): Observable<T>
+}
