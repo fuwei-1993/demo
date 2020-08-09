4bcb3b1e66f4620f2fca6152ca790a99e5765f70
fuwei <454575238@qq.com>
2020-08-10 07:38:06 +0800

    feat(redux): complete bindActionCreator

---
+++ redux/bindActionCreator.ts
@@ -0,0 +1,77 @@
+import {
+  AnyAction,
+  ActionCreator,
+  ActionCreatorsMapObject,
+} from './types/action'
+import { Dispatch } from './types/store'
+
+function bindActionCreator<A extends AnyAction = AnyAction>(
+  actionCreator: ActionCreator<A>,
+  dispatch: Dispatch
+) {
+  return function (this: any, ...args: any[]) {
+    return dispatch(actionCreator.apply(this, args))
+  }
+}
+
+/**
+ * @param actionCreators An object whose values are action
+ * creator functions. One handy way to obtain it is to use ES6 `import * as`
+ * syntax. You may also pass a single function.
+ *
+ * @param dispatch The `dispatch` function available on your Redux
+ * store.
+ *
+ * @returns The object mimicking the original object, but with
+ * every action creator wrapped into the `dispatch` call. If you passed a
+ * function as `actionCreators`, the return value will also be a single
+ * function.
+ */
+
+export function bindActionCreators<A, C extends ActionCreator<A>>(
+  actionCreator: C,
+  dispatch: Dispatch
+): C
+
+export function bindActionCreators<
+  A extends ActionCreator<any>,
+  B extends ActionCreator<any>
+>(actionCreator: A, dispatch: Dispatch): B
+
+export function bindActionCreators<A, M extends ActionCreatorsMapObject<A>>(
+  actionCreators: M,
+  dispatch: Dispatch
+): M
+
+export function bindActionCreators<
+  M extends ActionCreatorsMapObject,
+  N extends ActionCreatorsMapObject
+>(actionCreators: M, dispatch: Dispatch): N
+
+export function bindActionCreators(
+  actionCreators: ActionCreator<any> | ActionCreatorsMapObject,
+  dispatch: Dispatch
+) {
+  if (typeof actionCreators === 'function') {
+    return bindActionCreator(actionCreators, dispatch)
+  }
+
+  if (typeof actionCreators !== 'object' || actionCreators === null) {
+    throw new Error(
+      `bindActionCreators expected an object or a function, instead received ${
+        actionCreators === null ? 'null' : typeof actionCreators
+      }. ` +
+        `Did you write "import ActionCreators from" instead of "import * as ActionCreators from"?`
+    )
+  }
+
+  const boundActionCreators: ActionCreatorsMapObject = {}
+  for (const key in actionCreators) {
+    const actionCreator = actionCreators[key]
+    if (typeof actionCreator === 'function') {
+      boundActionCreators[key] = bindActionCreator(actionCreator, dispatch)
+    }
+  }
+
+  return boundActionCreators
+}
---
+++ redux/combineReducers.ts
@@ -0,0 +1,17 @@
+import { Action } from './types/action'
+
+function getUndefinedStateErrorMessage(key: string, action: Action) {
+  const actionType = action && action.type
+  const actionDescription =
+    (actionType && `action "${String(actionType)}"`) || 'an action'
+
+  return (
+    `Given ${actionDescription}, reducer "${key}" returned undefined. ` +
+    `To ignore an action, you must explicitly return the previous state. ` +
+    `If you want this reducer to hold no value, you can return null instead of undefined.`
+  )
+}
+
+function getUnexpectedStateShapeWarningMessage(
+  
+) {}
