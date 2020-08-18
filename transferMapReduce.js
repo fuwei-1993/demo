2902dac6e58505e16c5837dd076b7a81a95132ff
fuwei <454575238@qq.com>
2020-08-18 17:33:15 +0800

    feat: analysis the reducer problem of quote

--- transferMapReduce.js
+++ transferMapReduce.js
@@ -3,8 +3,8 @@ let arr = [1, 2, 3, 4]
 arr._map = function (fn, thisArg) {
   const result = []
   const a = this.reduce((prev, cur, curIndex, array) => {
-    result.push(fn.call(thisArg, cur,curIndex, array))
-      return prev
+    result.push(fn.call(thisArg, cur, curIndex, array))
+    return prev
   })
   return result
 }
@@ -16,7 +16,7 @@ const mockMapResult = arr._map((item, index) => {
 
 console.log(mockMapResult)
 
-let arr = [1, 23, 4, 5]
+arr = [1, 23, 4, 5]
 
 arr._reduce = function (fn, initValue) {
   let resultVal = initValue || this[0]
@@ -31,9 +31,28 @@ arr._reduce = function (fn, initValue) {
   return resultVal
 }
 
-const mockReduceResult = arr._reduce((prev,cur) => {
+const mockReduceResult = arr._reduce((prev, cur) => {
   console.log(prev, cur)
   return prev.concat(cur)
-},[])
+}, [])
 
 console.log(mockReduceResult)
+
+let reducer = 234
+
+const middlewareAPI = {
+  reducer: () => reducer,
+}
+
+function compose({ reducer }) {
+  return (next) => {}
+}
+console.log(middlewareAPI.reducer())
+reducer = compose(middlewareAPI)
+console.log(middlewareAPI.reducer())
+reducer()
+let test = 1
+const obj = { test: () => test }
+
+test = 2
+console.log('-------',obj.test())
