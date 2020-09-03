038c2775da50b3d9ce9a536d3c31fa8ebac97759
fuwei <454575238@qq.com>
2020-09-04 07:49:53 +0800

    feat(ts): add types for indexof libery

---
+++ typescript/index.ts
@@ -0,0 +1,8 @@
+/**
+ * 为第三方写声明文件
+ */
+
+import indexOf = require('indexOf')
+
+
+console.log(indexOf)
---
+++ typescript/typings/indexOf.d.ts
@@ -0,0 +1,4 @@
+declare function indexOf<T>(array: Array<T>, value: any): T | -1
+
+
+export = indexOf 
