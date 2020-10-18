23511333f959e2a2f39bba4a34ff335fa5fdcaa2
fuwei <454575238@qq.com>
2020-10-18 18:37:51 +0800

    feat: add insert sort

--- algorithm/index.js
+++ algorithm/index.js
@@ -3,15 +3,35 @@ function fisherYatesShuffle(arr) {
   //   const j = i + Math.floor(Math.random() * (arr.length - i))
   //   [arr[i], arr[j]] = [arr[j], arr[i]]
   // }
-  
+
   // return arr
   const i = 1
-  const j = 2
-  [arr[0], arr[1]] = [arr[1], arr[0]]
+  const j = ((2)[(arr[0], arr[1])] = [arr[1], arr[0]])
   return arr
 }
 
-const arr = [1,2,3,4,5,6,7,8]
-
+const arr = [1, 2, 3, 4, 5, 6, 7, 8]
 
 console.log(fisherYatesShuffle(arr))
+
+// 插入排序 理论上是一个个插进新的数组
+
+function insertSort(arr) {
+  const result = arr.slice()
+
+  for (let i = 1; i < arr.length; i++) {
+    const curr = arr[i];
+    let j = i - 1
+
+    while ( j >= 0 && curr < result[j]) {
+      result[j + 1] = result[j]
+      j--
+    }
+    result[j + 1] = curr
+  }
+
+  return result
+}
+
+console.log(insertSort([1, 2, 22, 1, 222, 33, 4, 5]))
+
