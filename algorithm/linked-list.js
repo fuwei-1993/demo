086f605216208e15222f4d4688ff973a82e67afb
fuwei <454575238@qq.com>
2020-12-09 14:26:42 +0800

    feat: add linked list with the all methods

--- algorithm/linked-list.js
+++ algorithm/linked-list.js
@@ -1,7 +1,4 @@
-function defaultEquals(a, b) {
-  return a === b
-}
-
+// 单向链表
 class Node {
   constructor(element) {
     this.element = element
@@ -12,6 +9,10 @@ class Node {
 class LinkedList {
   head = null
   count = 0
+
+  equalsFn(a, b) {
+    return a === b
+  }
 
   push(element) {
     const node = new Node(element)
@@ -28,9 +29,30 @@ class LinkedList {
 
     this.count++
   }
-  
-  insert() {}
-  
+
+  insert(element, index) {
+    if (index < 0 || index >= this.count) return false
+
+    const node = new Node(element)
+
+    this.count++
+
+    if (index === 0) {
+      node.next = this.head
+      this.head = node
+
+      return true
+    }
+
+    const previous = this.getElementAt(index - 1)
+    const current = previous.next
+
+    previous.next = node
+    node.next = current
+
+    return true
+  }
+
   getElementAt(index) {
     // 返回链表中指定位置的元素，没有就是 undefined
     if (index < 0 || index >= this.count) return undefined
@@ -43,9 +65,36 @@ class LinkedList {
     return node
   }
 
-  remove() {}
+  remove() {
+    if (!this.count) return undefined
 
-  indexOf() {}
+    let current = this.head
+    let previous
+
+    while (current.next) {
+      previous = current
+      current = current.next
+    }
+
+    previous.next = undefined
+    this.count--
+    return previous.next
+  }
+
+  indexOf(element) {
+    // 根据元素来查找出 index
+    let current = this.head
+
+    for (let i = 0; i < this.count; i++) {
+      if (this.equalsFn(element, current.element)) {
+        return i
+      } else {
+        current = current.next
+      }
+    }
+
+    return -1
+  }
 
   removeAt(index) {
     if (index < 0 || index >= this.count) return undefined
@@ -66,11 +115,31 @@ class LinkedList {
     return current
   }
 
-  isEmpty() {}
+  isEmpty() {
+    return this.size() === 0
+  }
 
-  size() {}
+  size() {
+    return this.count
+  }
 
-  toString() {}
+  getHead() {
+    return this.head
+  }
+
+  toString() {
+    if (!this.size()) return ''
+
+    let current = this.head
+    let objString = ''
+
+    for (let i = 0; i < this.size() && current; i++) {
+      objString += `${current.element},`
+      current = current.next
+    }
+
+    return objString
+  }
 }
 
 const list = new LinkedList()
@@ -79,4 +148,8 @@ list.push(15)
 list.push(10)
 
 list.removeAt(2)
-console.log(list)
+
+list.insert(11, 0)
+
+console.log(list.indexOf(15), list.indexOf(11))
+console.log(list.toString())
