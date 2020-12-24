ac5de2d4dd561f17819d7be7388f74e1c0302e0b
fuwei <454575238@qq.com>
2020-12-24 18:25:24 +0800

    feat: add leetcode fielder & intersect

---
+++ algorithm/leetcode/两个数组的交集/index.js
@@ -0,0 +1,46 @@
+function intersect(numbers1, numbers2) {
+  const numMap = {}
+  for (let i = 0; i < numbers1.length; i++) {
+    if (numMap[numbers1[i]]) {
+      numMap[numbers1[i]]++
+    } else {
+      numMap[numbers1[i]] = 1
+    }
+  }
+
+  for (let i = 0; i < numbers2.length; i++) {
+    if (numMap[numbers2[i]]) {
+      numMap[numbers2[i]]--
+    }
+  }
+
+  return numMap
+}
+
+// console.log(intersect([1, 2, 3], [3, 4, 5]))
+
+//  题目在进阶问题中问道：如果给定的数组已经排好序呢？你将如何优化你的算法？我们分析一下，
+//  假如两个数组都是有序的，分别为：arr1 = [1,2,3,4,4,13]，arr2 = [1,2,3,9,10]
+// 双指针解法： 比较两个指针是否相等 不相等就后移小的那个 相等就装进数组
+
+function intersect2(numbers1, numbers2) {
+  let i = 0,
+    j = 0,
+    k = 0
+  while (i < numbers1.length && j < numbers2.length) {
+    if (numbers1[i] < numbers2[j]) {
+      i++
+    } else if (numbers1[i] > numbers2[j]) {
+      j++
+    } else if (numbers1[i] === numbers2[j]) {
+      result.push(numbers1[i])
+      k++
+      j++
+      i++
+    }
+  }
+
+  return result
+}
+
+console.log(intersect2([1, 2, 3, 4, 4, 13], [1, 2, 3, 9, 10]))
