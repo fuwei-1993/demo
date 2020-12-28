bd412c0904f93dccee9f7fe5aa38181da3e2ad7c
fuwei <454575238@qq.com>
2020-12-28 20:59:02 +0800

    fix(leetcode): three sum isnt resolved the repeat problem

--- algorithm/leetcode/三数之和/index.js
+++ algorithm/leetcode/三数之和/index.js
@@ -1,19 +1,15 @@
 // 第15题：三数之和
 // 给你一个包含 n 个整数的数组 numbers numbers 中是否存在三个元素 a，b，c ，
 // 使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。注意：答案中不可以包含重复的三元组。
-// 给定数组 numbers = [-1, 0, 1, 2, -1, -4]，
+// 给定数组 numbers = [-1,0,1,2,-1,-4,-2,-3,3,0,4]
 // 满足要求的三元组集合为：
-// [
-//   [-1, 0, 1],
-//   [-1, -1, 2]
-// ]
+// [[-4,0,4],[-4,1,3],[-3,-1,4],[-3,0,3],[-3,1,2],[-2,-1,3],[-2,0,2],[-1,-1,2],[-1,0,1]]
 
 // 利用双指针的问题来求解此问题
 function threeSum(numbers) {
   const sortedNumbers = numbers.sort((a, b) => a - b)
-  let l,
-    r = numbers.length - 1,
-    result = []
+
+  let result = []
 
   function steps(currentStep, step) {
     let stepNum = currentStep + step
@@ -23,26 +19,28 @@ function threeSum(numbers) {
     return stepNum
   }
 
-  for (let i = 0; i < numbers.length; i++) {
+  for (let i = 0; i < sortedNumbers.length; i++) {
     const fixedOne = sortedNumbers[i]
-    l = i + 1
+    let l = i + 1,
+      r = sortedNumbers.length - 1
 
     if (fixedOne > 0) return result
-    if (sortedNumbers[i] === sortedNumbers[i + 1]) {
-      i++
-    }
-    while (r > l) {
-      if (fixedOne + sortedNumbers[r] + sortedNumbers[l] > 0) {
-        r = steps(r, -1)
-      } else if (fixedOne + sortedNumbers[r] + sortedNumbers[l] < 0) {
-        l = steps(l, 1)
-      } else {
-        result.push([fixedOne, sortedNumbers[l], sortedNumbers[r]])
-        l = steps(l, 1)
+    if (sortedNumbers[i] != sortedNumbers[i - 1]) {
+      while (r > l) {
+        if (sortedNumbers[r] + sortedNumbers[l] === -fixedOne) {
+          result.push([fixedOne, sortedNumbers[l], sortedNumbers[r]])
+
+          l = steps(l, 1)
+          r = steps(r, -1)
+        } else if (sortedNumbers[r] + sortedNumbers[l] < -fixedOne) {
+          l++
+        } else {
+          r--
+        }
       }
     }
   }
   return result
 }
 
-console.log(threeSum([-1, 0, 1, 1, 2, -1, -1, -1, -4]))
+console.log(threeSum([-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6]))
