87979a9260a9bfcc5cc02333f8c69901a407150d
fuwei <454575238@qq.com>
2021-02-19 21:17:46 +0800

    feat(algorithm): add search algorithm of Max Area of Island

--- algorithm/leetcode/有序数组中的单一元素/index.js
+++ algorithm/leetcode/有序数组中的单一元素/index.js
@@ -10,3 +10,22 @@
 // 输入: [3,3,7,7,10,11,11]
 // 输出: 10
 // 注意: 您的方案应该在 O(log n)时间复杂度和 O(1)空间复杂度中运行。
+
+function singleNonDuplicate(numbers) {
+  let start = 0, end = numbers.length - 1
+
+  while(start < end) {
+    let mid = Math.floor((start + end) / 2)
+
+    if(mid % 2 === 1) mid--
+    if(numbers[mid] === numbers[mid + 1]) {
+      start = mid + 2
+    } else {
+      end = mid
+    }
+  }
+
+  console.log(start)
+}
+
+singleNonDuplicate([1,1,2,3,3,4,4,8,8])
