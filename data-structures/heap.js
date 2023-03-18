b993328b0de59c05e70aa6f88ba4d0d5a86d53d1
fuwei <454575238@qq.com>
2023-03-18 16:54:50 +0800

    feat: add extract for min-heap

--- data-structures/heap.js
+++ data-structures/heap.js
@@ -45,6 +45,43 @@ class MinHeap {
     target[parentIndex] = temp
   }
 
+  shiftDown(index) {
+    let current = index
+    const left = this.findLeftIndex(index)
+    const right = this.findRightIndex(index)
+
+    if (
+      left < this.size() &&
+      this.compareFn(this.heap[left], this.heap[current]) === Compare.LESS_THAN
+    ) {
+      current = left
+    }
+    if (
+      right < this.size() &&
+      this.compareFn(this.heap[right], this.heap[current] === Compare.LESS_THAN)
+    ) {
+      current = right
+    }
+    if (current !== index) {
+      this.swap(this.heap, index, current)
+      this.shiftDown(current)
+    }
+  }
+
+  extract() {
+    if (this.isEmpty()) {
+      return
+    }
+    if (this.size() <= 1) {
+      return this.heap.pop()
+    }
+
+    const removed = this.heap[0]
+    this.heap[0] = this.heap.pop()
+    this.shiftDown(0)
+    return removed
+  }
+
   insert(value) {
     if (this.isEmpty()) {
       this.heap.push(value)
@@ -67,12 +104,15 @@ class MinHeap {
 const minHeap = new MinHeap()
 
 minHeap.insert(1)
-minHeap.insert(0)
-minHeap.insert(5)
 minHeap.insert(3)
-minHeap.insert(4)
-minHeap.insert(6)
 minHeap.insert(2)
-minHeap.insert(7)
+minHeap.insert(4)
+minHeap.insert(5)
 
-console.log(minHeap);
+
+
+console.log(minHeap)
+
+minHeap.extract()
+
+console.log(minHeap)
