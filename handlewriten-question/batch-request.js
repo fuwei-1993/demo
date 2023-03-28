db2372534edb113c34b3188716d6ae3c7247d244
fuwei <454575238@qq.com>
2023-03-28 17:40:07 +0800

    feat: add concurrentRequestsControl

--- handlewriten-question/batch-request.js
+++ handlewriten-question/batch-request.js
@@ -1,3 +1,4 @@
+// 此方法不对
 async function batchRequest(requests, numbers) {
   let reqCells = []
   let result = []
@@ -13,15 +14,63 @@ async function batchRequest(requests, numbers) {
 
   for (const req of reqCells) {
     try {
-      const r = await new Promise.all(req)
+      const r = await new Promise.race(req)
       result.push(...r)
     } catch (error) {
       errors.push(error)
     }
-
   }
   return {
     result,
-    errors
+    errors,
   }
 }
+
+function mockRequest(message) {
+  console.log(`发送${message}`)
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(message)
+    }, 1000)
+  })
+}
+
+function makeReqTasks(mocker, count) {
+  const result = []
+  for (let i = 0; i < count; i++) {
+    result.push(mocker)
+  }
+
+  return result
+}
+
+// TODO..
+async function concurrentRequestsControl(tasks, limit) {
+  const currentTasks = []
+  const res = []
+
+  for (let i = 0; i < tasks.length; i++) {
+    const cur = tasks[i](i)
+      .then((res) => {
+        console.log(`${res}请求成功`)
+        res[i] = res
+        currentTasks.splice(i, 1)
+      })
+      .catch((err) => {
+        res[i] = err
+        currentTasks.splice(i, 1)
+      })
+
+    currentTasks.push(cur)
+    if (currentTasks.length == limit) {
+      await Promise.race(currentTasks)
+    }
+  }
+
+  return Promise.allSettled(currentTasks)
+}
+const tasks = makeReqTasks(mockRequest, 10)
+// batchRequest(tasks, 3)
+concurrentRequestsControl(tasks, 1).then((res) => {
+  //   console.log(`这是第${res}个请求`)
+})
