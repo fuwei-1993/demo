3f83873d2b92a0ec9765bacd2a272a9af891b2d8
fuwei <454575238@qq.com>
2024-06-10 22:00:47 +0800

    feat: init leetcode 108

---
+++ algorithm/leetcode2024/108. 将有序数组转换为二叉搜索树/index.js
@@ -0,0 +1,14 @@
+class TreeNode {
+  constructor(val, left = null, right = null) {
+    this.val = val
+    this.left = left
+    this.right = right
+  }
+}
+
+/* @param {number[]} nums
+* @return {TreeNode}
+*/
+var sortedArrayToBST = function(nums) {
+
+};
