46884cab83258439ddd01be5ff2eeca404de596e
fuwei <454575238@qq.com>
2024-06-17 22:21:27 +0800

    feat: 108. Convert Sorted Array to Binary Search Tree

--- algorithm/leetcode2024/108. 将有序数组转换为二叉搜索树/index.js
+++ algorithm/leetcode2024/108. 将有序数组转换为二叉搜索树/index.js
@@ -1,14 +1,30 @@
 class TreeNode {
-  constructor(val, left = null, right = null) {
-    this.val = val
-    this.left = left
-    this.right = right
-  }
+	constructor(val, left = null, right = null) {
+		this.val = val
+		this.left = left
+		this.right = right
+	}
 }
 
-/* @param {number[]} nums
-* @return {TreeNode}
-*/
-var sortedArrayToBST = function(nums) {
+/**
+ *
+ * @param {number[]} nums
+ * @returns {TreeNode}
+ */
+function sortedArrayToBST(nums) {
+	return helper(nums, 0, nums.length - 1)
+}
 
-};
+function helper(nums, left, right) {
+	if (right < left) return null
+
+	const mid = Math.floor((left + right) / 2)
+	const root = new TreeNode(nums[mid] ?? null)
+
+	root.left = helper(nums, left, mid - 1)
+	root.right = helper(nums, mid + 1, right)
+
+	return root
+}
+
+console.log(sortedArrayToBST([1, 2, 3, 4, 5]))
